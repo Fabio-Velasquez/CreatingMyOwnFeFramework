f8c7a418a5371d59287014d0bb49b366001d6fe7
Fabio-Velasquez <velasquezfabio2000@gmail.com>
2025-12-06 18:24:52 -0600

    feature: first version of framework completed.

--- packages/runtime/src/app.js
+++ packages/runtime/src/app.js
@@ -1,36 +1,47 @@
 import {destroyDOM} from './destroy-dom'
 import {mountDOM} from './mount-dom'
+import {Dispatcher} from "./dispatcher";
 
 export function createApp({state, view,  reducers = {}}) {
-    let parentEl = null
-    let vdom = null
+    let parentEl = null;
+    let vdom = null;
 
 
-    const dispatcher = new Dispatcher()
-    const subscriptions = [dispatcher.afterEveryCommand(renderApp)]
+    const dispatcher = new Dispatcher();
+    const subscriptions = [dispatcher.afterEveryCommand(renderApp)];
+
+
+    function emit(eventName, payload) {
+        dispatcher.dispatch(eventName, payload);
+    }
 
     for (const actionName in reducers) {
-        const reducer = reducers[actionName]
+        const reducer = reducers[actionName];
 
         const subs = dispatcher.subscribe(actionName, (payload) => {
-            state = reducer(state, payload)
-        })
-        subscriptions.push(subs)
+            state = reducer(state, payload);
+        });
+        subscriptions.push(subs);
     }
 
     function renderApp() {
         if (vdom) {
-            destroyDOM(vdom)
+          destroyDOM(vdom);
         }
 
-        vdom = view(state)
+        vdom = view(state, emit);
         mountDOM(vdom, parentEl)
     }
 
     return {
         mount(_parentEl) {
-            parentEl = _parentEl
-            renderApp()
+            parentEl = _parentEl;
+            renderApp();
         },
-    }
+        unmount() {
+            destroyDOM(vdom)
+            vdom = null
+            subscriptions.forEach((unsubscribe) => unsubscribe())
+        },
+    };
 }
