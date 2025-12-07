655ce19eba8a20243ed79cd9d0feffba7cdafa87
Fabio-Velasquez <velasquezfabio2000@gmail.com>
2025-12-07 13:45:20 -0600

    docs: documented app.js

--- packages/runtime/src/app.js
+++ packages/runtime/src/app.js
@@ -2,11 +2,18 @@ import {destroyDOM} from './destroy-dom'
 import {mountDOM} from './mount-dom'
 import {Dispatcher} from "./dispatcher";
 
+/**
+ *
+ * @param state inital state of the appplication
+ * @param view the top level component of the app
+ * @param reducers object that maps command names to reducer functions
+ * @returns {{mount(*): void, unmount(): void}}
+ */
 export function createApp({state, view,  reducers = {}}) {
     let parentEl = null;
     let vdom = null;
 
-
+    // created the dispatcher object so we can render view based on interactions
     const dispatcher = new Dispatcher();
     const subscriptions = [dispatcher.afterEveryCommand(renderApp)];
 
@@ -24,6 +31,10 @@ export function createApp({state, view,  reducers = {}}) {
         subscriptions.push(subs);
     }
 
+    /**
+     * renders the view, it will check if vdom exists destroys the DOM view
+     * and process to regenerate the new DOM view.
+     */
     function renderApp() {
         if (vdom) {
           destroyDOM(vdom);
@@ -32,7 +43,9 @@ export function createApp({state, view,  reducers = {}}) {
         vdom = view(state, emit);
         mountDOM(vdom, parentEl)
     }
-
+    /*
+    returns an object with methods to mount and unmount DOM
+     */
     return {
         mount(_parentEl) {
             parentEl = _parentEl;
