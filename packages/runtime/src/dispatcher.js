15c731f353f893e7d00a7aa9cf6434f83c4904c8
Fabio-Velasquez <velasquezfabio2000@gmail.com>
2025-12-07 13:16:31 -0600

    docs: documented dispatcher class

--- packages/runtime/src/dispatcher.js
+++ packages/runtime/src/dispatcher.js
@@ -1,12 +1,36 @@
+/**
+ *
+ * Dispatcher responsible to associate commands
+ * with handler function through subscription logic which in turn
+ * the handler will call the respected command logic
+ *
+ */
 export class Dispatcher {
+    // tracks all cmd:consumer subscriptions
     #subs = new Map();
     #afterHandlers = []
+
+    /**
+     * Creates an empty array of handlers if none exist
+     * adds handler to the array
+     * returns empty function if handler exists
+     * or
+     * returns function to unregister handler
+     * @param commandName cmd name application developer will use
+     * @param handler handler function logic to execute command logic
+     * @returns {(function(): void)|*|(function())}
+     */
     subscribe(commandName, handler) {
         if(!this.#subs.has(commandName)) {
             this.#subs.set(commandName,  []);
         }
 
         const handlers = this.#subs.get(commandName);
+        /*
+        return an empty function since handler is already registered
+        better to return an empty function instead of ubsubscribe the handler
+        app developer may call the same handler more than once.
+         */
         if(handlers.includes(handler)) {
             return () => {};
         }
@@ -16,6 +40,12 @@ export class Dispatcher {
             handlers.splice(idx, 1);
         };
     }
+
+    /**
+     * executed after every command to notify the renderer
+     * @param handler handler event
+     * @returns {(function(): void)|*} unsub handler when called.
+     */
     afterEveryCommand(handler) {
         this.#afterHandlers.push(handler);
         return () => {
@@ -24,6 +54,13 @@ export class Dispatcher {
         };
     }
 
+    /**
+     * dispatches the command name with respected handler function
+     * passed payload to create new state.
+     * Calls after handlers to rerender the view.
+     * @param commandName
+     * @param payload
+     */
     dispatch(commandName, payload) {
         if(this.#subs.has(commandName)) {
             this.#subs.get(commandName).forEach((handler) => {
